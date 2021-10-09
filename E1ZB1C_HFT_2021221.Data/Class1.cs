ce1b6811c8028a4fdd99bb5ec95b13865624c98e
bnyma <bnyma@DESKTOP-5IJLNI5>
2021-10-09 17:54:52 +0200

    Add project files.

---
+++ E1ZB1C_HFT_2021221.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace E1ZB1C_HFT_2021221.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ E1ZB1C_HFT_2021221.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace E1ZB1C_HFT_2021221.Models
+{
+    public class Class1
+    {
+    }
+}
