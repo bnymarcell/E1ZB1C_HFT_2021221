966b92f55dc1ce731dbe656a44c491d47159adf8
bnymarcell@gmail.com <bnymarcell@gmail.com>
2021-11-28 10:42:59 +0100

    All controllers are done.

--- E1ZB1C_HFT_2021221.Endpoint/Controllers/StatController.cs
+++ E1ZB1C_HFT_2021221.Endpoint/Controllers/StatController.cs
@@ -1,4 +1,5 @@
 ﻿using E1ZB1C_HFT_2021221.Logic;
+using E1ZB1C_HFT_2021221.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -8,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace E1ZB1C_HFT_2021221.Endpoint.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]/[action]")]
     [ApiController]
     public class StatController : ControllerBase
     {
@@ -22,6 +23,49 @@ namespace E1ZB1C_HFT_2021221.Endpoint.Controllers
             this.carl = carl;
             this.dl = dl;
         }
-        
+
+
+        // GET: /stat/carcount/
+        [HttpGet("{id}")]
+        public IEnumerable<string> Carcount(int id)
+        {
+            return cl.CarCount(id);
+        }
+
+        // /stat/howmany
+        [HttpGet("{id}")]
+        public IEnumerable<KeyValuePair<string, int>> HowMany(int id)
+        {
+            return cl.HowMany(id);
+        }
+
+        // GET: /stat/idavg
+        [HttpGet]
+        public IEnumerable<KeyValuePair<string, double>> IDAVG()
+        {
+            return cl.IDAVG();
+        }
+
+        // GET: /stat/driveswhat
+        [HttpGet("{id}")]
+        public IEnumerable<string> DrivesWhat(int id)
+        {
+            return dl.DrivesWhat(id);
+        }
+
+        // GET: /stat/whodrives
+        [HttpGet("{id}")]
+        public IEnumerable<string> WhoDrives(int id)
+        {
+            return carl.WhoDrives(id);
+        }
+
+
+        // GET: /stat/driversalary
+        [HttpGet("{id}")]
+        public IEnumerable<int> DriverSalary(int id)
+        {
+            return carl.DriverSalary(id);
+        }
     }
 }
