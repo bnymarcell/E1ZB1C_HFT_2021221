272c7b7628772f9d0ef40315fa0b768467e9d642
bnymarcell@gmail.com <bnymarcell@gmail.com>
2021-12-02 22:45:34 +0100

    Menu Still W.I.P

--- E1ZB1C_HFT_2021221.client/Program.cs
+++ E1ZB1C_HFT_2021221.client/Program.cs
@@ -31,11 +31,30 @@ namespace E1ZB1C_HFT_2021221.client
             Console.WriteLine("Choose a menupoint");
             Console.WriteLine(">1> Create a new Company, Car, or Driver");
             Console.WriteLine(">2> Read everything from selected table");
-            Console.WriteLine(">3> Update an existing item");
-            Console.WriteLine(">4> Delete and existing item");
-
+            Console.WriteLine(">3> Update an existing model");
+            Console.WriteLine(">4> Delete and existing model");
+            Console.WriteLine(">5> Read an item from a model");
+            Console.WriteLine(">6> Non Cruds");
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Create();
+                    return true;
+            }
         }
 
+        private static void Create()
+        {
+            Console.Clear();
+            RestService rest = new RestService("http://localhost:50212");
+            Console.WriteLine("Where do you want to create model");
+            string whereto = Console.ReadLine();
+            Console.Clear();
+            if ( whereto == "company")
+            {
+
+            }
+        }
     }
 }
     
